751baad84800ee466227c2747a59276460af98ba
TelmoAlexandre <37218412+TelmoAlexandre@users.noreply.github.com>
2019-08-04 15:27:12 +0100

    Listar utilizadores

--- LabFoto/LabFoto/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ LabFoto/LabFoto/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using LabFoto.APIs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -15,16 +16,16 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly IEmailSender _emailSender;
+        private readonly IEmailAPI _email;
 
         public IndexModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            IEmailSender emailSender)
+            IEmailAPI emailAPI)
         {
             _userManager = userManager;
             _signInManager = signInManager;
-            _emailSender = emailSender;
+            _email = emailAPI;
         }
 
         public string Username { get; set; }
@@ -42,10 +43,6 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
             [Required]
             [EmailAddress]
             public string Email { get; set; }
-
-            [Phone]
-            [Display(Name = "Telemóvel")]
-            public string PhoneNumber { get; set; }
         }
 
         public async Task<IActionResult> OnGetAsync()
@@ -64,8 +61,7 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                Email = email,
-                PhoneNumber = phoneNumber
+                Email = email
             };
 
             IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
@@ -83,7 +79,7 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound("Erro ao encontrar a sua conta não foi encontrada.");
             }
 
             var email = await _userManager.GetEmailAsync(user);
@@ -93,18 +89,7 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
                 if (!setEmailResult.Succeeded)
                 {
                     var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
-                }
-            }
-
-            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-            if (Input.PhoneNumber != phoneNumber)
-            {
-                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                if (!setPhoneResult.Succeeded)
-                {
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                    throw new InvalidOperationException("Erro ao encontrar o seu e-mail.");
                 }
             }
 
@@ -123,7 +108,7 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound("Erro ao encontrar a sua conta não foi encontrada.");
             }
 
 
@@ -135,12 +120,13 @@ namespace LabFoto.Areas.Identity.Pages.Account.Manage
                 pageHandler: null,
                 values: new { userId = userId, code = code },
                 protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
+            _email.Send(
+                email,
+                "Confirmar o e-mail",
+                $"Por facor confirme a sua conta: <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clique aqui</a>.");
+
+            StatusMessage = "E-mail de confirmação enviado. Verfique o seu e-mail.";
             return RedirectToPage();
         }
     }
---
+++ LabFoto/LabFoto/Areas/Identity/Pages/Account/Users.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabFoto.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LabFoto.Areas.Identity.Pages.Account
+{
+    [Authorize]
+    public class UsersModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<RegisterUserModel> _logger;
+
+        public UsersModel(ApplicationDbContext context,
+            UserManager<IdentityUser> userManager,
+            ILogger<RegisterUserModel> logger)
+            
+        {
+            _context = context;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IEnumerable<IdentityUser> Users { get; set; }
+
+        public void OnGet()
+        {
+            Users = _context.Users.Select(u => u).ToList();
+        }
+
+        public void OnPostSearch(string Username)
+        {
+            var users = _context.Users.Select(u => u);
+
+            if (!String.IsNullOrEmpty(Username))
+            {
+                users = users.Where(u => u.UserName.Contains(Username));
+            }
+
+            Users = users.ToList();
+        }
+    }
+}
