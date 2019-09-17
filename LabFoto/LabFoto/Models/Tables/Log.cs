902347d61279e4be0b6c6055935e506452d94884
Fabio19088 <37218406+Fabio19088@users.noreply.github.com>
2019-09-17 14:42:44 +0100

    tabela de logs

---
+++ LabFoto/LabFoto/Models/Tables/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabFoto.Models.Tables
+{
+    public class Log
+    {
+        [Key]
+        public string ID { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Classe { get; set; }
+
+        public string Metodo { get; set; }
+
+        public string Erro { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
