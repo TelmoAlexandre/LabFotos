efc1b6f41ed9f07d9ca13f7b8540bde8eb57e726
Fabio19088 <37218406+Fabio19088@users.noreply.github.com>
2019-09-17 16:05:24 +0100

    logs index e details

---
+++ LabFoto/LabFoto/Controllers/LogsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabFoto.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabFoto.Controllers
+{
+    public class LogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Logs
+        public ActionResult Index()
+        {
+            var logs = _context.Logs.ToList();
+
+            return View(logs);
+        }
+
+        // GET: Logs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var log = _context.Logs.Find(id);
+
+            if (log == null)
+            {
+                return NotFound();
+            }
+
+            return View(log);
+        }
+
+    }
+}
